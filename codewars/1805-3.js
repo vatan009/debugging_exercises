3d4b18bc4085ec81191beadb17b7f31347a5dd7e
Vatan009 <pawarvatan95@gmail.com>
2026-05-18 23:28:02 +0530

    1805 - added broken codes

---
+++ codewars/1805-3.js
@@ -0,0 +1,15 @@
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase() {
+  this.value++;
+};
+
+Counter.prototype.getValue() {
+  return this.value;
+};
+
+Counter.prototype.reset() {
+  this.value = 0;
+};
