ccd52824e5802759f167ab621e30f367e71d8432
Vatan009 <pawarvatan95@gmail.com>
2026-05-19 00:09:06 +0530

    1805 - 2 questions solved

--- codewars/1805-4.js
+++ codewars/1805-4.js
@@ -1,3 +1,11 @@
 function arrayPlusArray(arr1, arr2) {
-  return arr1 + arr2; //something went wrong
+  let sum = 0;
+  //   arr1.forEach((e)=>sum+=e);arr2.forEach((e)=>sum+=e);
+  // [...arr1,...arr2].forEach(e=>sum+=e);
+  return [...arr1, ...arr2].reduce((sum, n) => sum + n, 0);
+
+  return sum; //something went wrong
+  //   concept : (arr1+arr2).forEach is invalid operation -> it converts [1,3][4,5] -> "1,34,5"
+  //   concept : reduce function gives single value as in return
+  //            : syntac : const sum = arr.redunce((finalSum,currentEle)=>finalSum+=currentElement,initial value)
 }
