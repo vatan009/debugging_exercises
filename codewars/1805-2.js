09be5ca31ce57e1bb73a750efcbdc152ba99996b
Vatan009 <pawarvatan95@gmail.com>
2026-05-18 23:57:57 +0530

    1805 - 3 questions solved

--- codewars/1805-2.js
+++ codewars/1805-2.js
@@ -1,6 +1,8 @@
 function swapValues() {
   var args = Array.prototype.slice.call(arguments);
-  var temp = args[0];
-  args[0] = args[1];
-  args[1] = temp;
+  console.log(args);
+  let temp = args[0][0];
+  args[0][0] = args[0][1];
+  args[0][1] = temp;
+  //concept : array.prototype.slice.call(argumnet) converts arguments into a array so that we can use map,filter
 }
--- codewars/1805-3.js
+++ codewars/1805-3.js
@@ -2,14 +2,16 @@ function Counter() {
   this.value = 0;
 }
 
-Counter.prototype.increase() {
+Counter.prototype.increase = function () {
   this.value++;
 };
 
-Counter.prototype.getValue() {
+Counter.prototype.getValue = function () {
   return this.value;
 };
 
-Counter.prototype.reset() {
+Counter.prototype.reset = function () {
   this.value = 0;
 };
+//concept : A.prototype.functionName=function(){ }
+//prototype is used to inherit the properties of A
