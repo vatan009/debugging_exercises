690fc6a774d48d59d720d7fbdf3ca25f7ce9487c
Vatan009 <pawarvatan95@gmail.com>
2026-05-19 23:31:40 +0530

    1905 added 5 questions

---
+++ codewars/1905-8.js
@@ -0,0 +1,18 @@
+function evalObject(value) {
+  var result = 0;
+  switch (value.operation) {
+    case "+":
+      result = value.a + value.b;
+    case "-":
+      result = value.a - value.b;
+    case "/":
+      result = value.a / value.b;
+    case "*":
+      result = value.a * value.b;
+    case "%":
+      result = value.a % value.b;
+    case "^":
+      result = Math.pow(value.a, value.b);
+  }
+  return result;
+}
