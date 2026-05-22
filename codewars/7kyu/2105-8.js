54ced99b0d2c13125dcade67de5ff3c725c6d73a
Vatan009 <pawarvatan95@gmail.com>
2026-05-22 23:40:28 +0530

    2205 : solved 3 questions

--- codewars/7kyu/2105-8.js
+++ codewars/7kyu/2105-8.js
@@ -3,6 +3,6 @@ let Warrior = function (name) {
   this.health = 100;
 };
 
-Warrior.strike = function (enemy, swings) {
+Warrior.prototype.strike = function (enemy, swings) {
   enemy.health = Math.max(0, enemy.health - swings * 10);
 };
